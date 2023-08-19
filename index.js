bf4b9419e9993b6b428cd7e0b13b57f71de893d3
Ireng Febrian <131000271+febriaan22@users.noreply.github.com>
2023-08-19 09:39:13 +0700

    Percobaan1

--- index.js
+++ index.js
@@ -33,16 +33,15 @@ const commonResponse = function (data, error) {
 //     database: process.env.MYSQL_DB
 // })
 //
-const mysqlCon1 = mysql.createConnection({
+
+const mysqlCon = mysql.createConnection({
     host: process.env.RAILWAY_HOST,
     port: process.env.RAILWAY_PORT,
     user: process.env.RAILWAY_USER,
     password: process.env.RAILWAY_PASS,
-    database: process.env.RAILWAY_DB,
-    URL: process.env.RAILWAY_URL
+    database: process.env.RAILWAY_DB
 })
 
-const mysqlCon = mysql.createConnection (`${process.env.RAILWAY_URL}`)
 const query = (query, values) => {
     return new Promise((resolve, reject) => {
         mysqlCon.query(query, values, (err, result, fields) => {
